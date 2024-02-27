e4c8052193fe5e8c72399794bc29bd22e56555bb
pedro-batista <peddrobatista2022@gmail.com>
2024-02-27 12:39:07 -0300

    Segundo commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,28 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import entities.Product;
+
 public class Program {
 	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
 		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		list.forEach(cons);
+		
+		list.forEach(System.out::println);
 	}
 }
